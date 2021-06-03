02c5109fd5e33e9d304aa4bc35d9da5ea6dab9ae
Daniel Crespo (Zima) <daniel@zima.is>
2021-06-03 11:11:40 +0000

    change: banner w/ whatsapp link

--- components/ContactBanner.js
+++ components/ContactBanner.js
@@ -1,13 +1,14 @@
 import styles from '../styles/ContactBanner.module.css'
 
 import { faPhoneAlt} from '@fortawesome/free-solid-svg-icons'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const ContactBanner = () => {
     return (
         <div className={styles.wrapper}>
-            <p>Estamos aquí para ti <a href="tel:+34690093663">< FontAwesomeIcon icon={faPhoneAlt} className={styles.phoneLink} /><span className={styles.spanBold}>690 093 663</span></a></p>
+            <p>Estamos aquí para ti <a href="https://wa.me/34690093663">< FontAwesomeIcon icon={faWhatsapp} className={styles.phoneLink} /><span className={styles.spanBold}>690 093 663</span></a></p>
         </div>
     );
 }
