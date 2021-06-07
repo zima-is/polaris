390d239f8760af280c2ce2c3051f73c30e83ae81
Daniel Crespo (Zima) <daniel@zima.is>
2021-06-07 14:33:05 +0000

    add: Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  
   return (
       <Layout>
         <Component {...pageProps} />
